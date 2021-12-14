ecbced10fa5f11824553c435729f11f275d88acc
Huide Zhou <prettyage.new@gmail.com>
2021-12-14 22:38:05 +0800

    guessing number

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,39 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn guess_number() {
+    //print!("Prepare a secret number: ");
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    //println!("{}", secret_number);
+
+    println!("Game on: guess the number!");
+
+    loop {
+        println!("Please input your guess:");
+
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("Failed to read a line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please input a number, try again!");
+                continue;
+            }
+        };
+        print!("Your guess is {}: ", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
 
 fn main() {
-    println!("Guess the number!");
-    println!("Please input your guess:");
-    
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess).expect("Failed to read a line");
-
-    println!("Your guess is {}", guess);
+    guess_number();
 }
