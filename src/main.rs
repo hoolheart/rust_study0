86ab7f5ac12060c29cb899fca77e2503b804b687
Huide Zhou <prettyage.new@gmail.com>
2022-02-24 18:38:40 +0800

    use FnOnce to cover closure and function

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,12 @@ trait TestCase {
     fn invoke(&self);
 }
 
-struct TestFunction<T> where T: Fn(), {
-    foo: T,
+struct TestFunction<'a, T: FnOnce() -> () + Copy> {
+    foo: &'a T,
     permit: bool,
 }
 
-impl<T> TestCase for TestFunction<T> where T: Fn(), {
+impl<'a, T> TestCase for TestFunction<'a, T> where T: FnOnce() -> () + Copy, {
     fn invoke(&self) {
         if self.permit {
             (self.foo)();
@@ -17,14 +17,14 @@ impl<T> TestCase for TestFunction<T> where T: Fn(), {
     }
 }
 
-impl<T> TestFunction<T> where T: Fn(), {
-    fn valid(foo: T) -> TestFunction<T> {
+impl<'a, T> TestFunction<'a, T> where T: FnOnce() -> () + Copy, {
+    fn valid(foo: &'a T) -> TestFunction<'a, T> {
         TestFunction {
             foo: foo,
             permit: true,
         }
     }
-    fn invalid(foo: T) -> TestFunction<T> {
+    fn invalid(foo: &'a T) -> TestFunction<'a, T> {
         TestFunction {
             foo: foo,
             permit: false,
@@ -33,16 +33,16 @@ impl<T> TestFunction<T> where T: Fn(), {
 }
 
 fn main() {
-    let chances = 10;
-
     let cases: Vec<Box<dyn TestCase>> = vec![
-        Box::new(TestFunction::valid(||{
+        Box::new(TestFunction::valid(&||{
+            let chances = 3;
             match guess_number(true, chances).1 {
                 true => println!("Succeed to guess the number"),
                 false => println!("Failed to guess the number in {} times", chances)
             }
         })),
-        Box::new(TestFunction::invalid(||{
+        Box::new(TestFunction::invalid(&||{
+            let chances = 10;
             let (answer, result) = guess_number(false, chances);
             if result {
                println!("Succeed to guess the number");
@@ -50,28 +50,28 @@ fn main() {
                println!("Failed to guess {} in {} times", answer, chances);
             }
         })),
-        Box::new(TestFunction::invalid(||{
+        Box::new(TestFunction::invalid(&||{
             test_variables();
             test_const();
         })),
-        Box::new(TestFunction::invalid(|| {test_types();})),
-        Box::new(TestFunction::invalid(|| {test_ownership();})),
-        Box::new(TestFunction::invalid(|| {test_struct();})),
-        Box::new(TestFunction::invalid(|| {test_enum();})),
-        Box::new(TestFunction::invalid(|| {
+        Box::new(TestFunction::invalid(&test_types)),
+        Box::new(TestFunction::invalid(&test_ownership)),
+        Box::new(TestFunction::invalid(&test_struct)),
+        Box::new(TestFunction::invalid(&test_enum)),
+        Box::new(TestFunction::invalid(&|| {
             test_vector();
             test_string();
             test_hash();
         })),
-        Box::new(TestFunction::invalid(|| {test_generic();})),
-        Box::new(TestFunction::valid(|| {
+        Box::new(TestFunction::invalid(&test_generic)),
+        Box::new(TestFunction::valid(&|| {
             let args = test_arguments(); // get argument list
             let file_name = parse_arg(&args); // get file name from arguments
             if let Err(e) = test_file(&file_name) {
                 eprintln!("{:?}", e);
             }
         })),
-        Box::new(TestFunction::valid(|| {test_closure();})),
+        Box::new(TestFunction::valid(&test_closure)),
     ];
 
     for case in cases {
